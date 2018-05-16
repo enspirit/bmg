e944cd9ae9dacdae2cb227ccfb3ba9456dfcda4b
Bernard Lambeau <blambeau@gmail.com>
2018-05-16 08:21:53 +0200

    Bump to 0.8.0 and release.

--- lib/bmg/version.rb
+++ lib/bmg/version.rb
@@ -1,8 +1,8 @@
 module Bmg
   module Version
     MAJOR = 0
-    MINOR = 7
-    TINY  = 1
+    MINOR = 8
+    TINY  = 0
   end
   VERSION = "#{Version::MAJOR}.#{Version::MINOR}.#{Version::TINY}"
 end
