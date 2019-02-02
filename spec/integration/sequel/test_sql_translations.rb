0f5fbef5339b9de4495829807dc419bef72aae5d
Bernard Lambeau <blambeau@gmail.com>
2019-02-02 15:31:52 +0100

    Move suppliers and parts methods in SpecHelper itself.

--- spec/integration/sequel/test_sql_translations.rb
+++ spec/integration/sequel/test_sql_translations.rb
@@ -2,72 +2,8 @@ require 'spec_helper'
 module Bmg
   describe Sequel, "SQL translations" do
 
-    class Context
-
-      def initialize(sequel_db)
-        @sequel_db = sequel_db
-      end
-      attr_reader :sequel_db
-
-      def cities_type
-        Type::ANY
-          .with_attrlist([:city, :country])
-      end
-
-      def cities
-        Bmg.sequel(:cities, sequel_db, cities_type)
-      end
-
-      def suppliers_type
-        Type::ANY
-          .with_attrlist([:sid, :name, :city, :status])
-          .with_keys([[:sid]])
-          .with_typecheck
-      end
-
-      def suppliers
-        Bmg.sequel(:suppliers, sequel_db, suppliers_type)
-      end
-
-      def suppliers_dataset
-        Bmg.sequel(sequel_db[:suppliers], suppliers_type)
-      end
-
-      def parts_type
-        Type::ANY
-          .with_attrlist([:pid, :name, :color, :weight, :city])
-          .with_keys([[:pid]])
-          .with_typecheck
-      end
-
-      def parts
-        Bmg.sequel(:parts, sequel_db, parts_type)
-      end
-
-      def supplies_type
-        Type::ANY
-          .with_attrlist([:sid, :pid, :qty])
-          .with_keys([[:sid, :pid]])
-          .with_typecheck
-      end
-
-      def supplies
-        Bmg.sequel(:supplies, sequel_db, supplies_type)
-      end
-
-      def native_sids_of_suppliers_in_london
-        type = Bmg::Type.new.with_attrlist([:sid])
-        Bmg.sequel(sequel_db["SELECT sid FROM suppliers WHERE city = 'London'"], type)
-      end
-
-      def compile(test_case)
-        self.instance_eval(test_case.bmg)
-      end
-
-    end
-
     def compiled(test_case)
-      Context.new(sequel_db).compile(test_case)
+      SpecHelper::Context.new(sequel_db).compile(test_case)
     end
 
     def clean(sql)
