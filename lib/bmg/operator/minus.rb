d75847b4569e4207e581861c9cb88f14e9b0f308
Felix <felix@hinterstellar.io>
2024-05-15 20:15:20 +0200

    Optimize Minus operation

--- lib/bmg/operator/minus.rb
+++ lib/bmg/operator/minus.rb
@@ -21,15 +21,27 @@ module Bmg
 
       def each(&bl)
         return to_enum unless block_given?
-        initial = operands[0].to_a
+        initial = operands[0].to_set
         tuples = operands.drop(1).inject(initial) do |agg, op|
-          agg - op.to_a
+          agg - op.to_set
         end
         tuples.each(&bl)
       end
 
       def to_ast
         [ :minus ] + operands.map(&:to_ast)
+      end
+
+    protected ### optimization
+
+      def _minus(type, other)
+        return self if other.is_a?(Relation::Empty)
+          case other
+          when Minus
+            Minus.new(type, operands + other.operands)
+          else
+            Minus.new(type, operands + [other])
+          end
       end
 
     end # class Union
--- spec/unit/operator/test_minus.rb
+++ spec/unit/operator/test_minus.rb
@@ -9,7 +9,10 @@ module Bmg
           { a: 2 },
           { a: 3 },
           { a: 4 },
-          { a: 5 }
+          { a: 5 },
+          { a: 6 },
+          { a: 7 },
+          { a: 8 },
         ]
       }
 
@@ -22,13 +25,26 @@ module Bmg
 
       let(:r3) {
         [
-          { a: 3 }
+          { a: 3 },
+          { a: 5 }
+        ]
+      }
+
+      let(:r4) {
+        [
+          { a: 7 }
         ]
       }
 
       it 'works' do
-        difference = Minus.new(Type::ANY, [r1, r2, r3])
-        expect(difference.to_a).to eql([{ a: 2 },{ a: 5 }])
+        difference = Minus.new(Type::ANY, [r1, r2, r3, r4])
+        expect(difference.to_a).to eql(
+          [
+            { a: 2 },
+            { a: 6 },
+            { a: 8 }
+          ]
+        )
       end
 
     end
